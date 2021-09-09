cdc7b0f876aff269518a65cb2deea584178f1469
HudSal <huda.abusal@gmail.com>
2021-09-09 15:05:51 +1000

    Initial

---
+++ ReservationRestaurant/ReservationRestaurant/Data/Reservation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReservationRestaurant.Data
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public int Duration { get; set; } // in miuntes
+        public DateTime EndTime { get => StartTime.AddMinutes(Guests); }
+        public int Guests { get; set; }
+
+
+        public List<Table> Tables { get; set; } = new List<Table>();
+
+        public int ReservationStatusId { get; set; }
+        public ReservationStatus ReservationStatus { get; set; }
+
+        public int ReservationOriginId { get; set; }
+        public ReservationOrigin ReservationOrigin { get; set; }
+
+
+        public int SittingId { get; set; }
+        public Sitting Sitting { get; set; }
+
+        public int PersonId { get; set; }
+        public Person Person { get; set; }
+
+
+    }
+}
---
+++ ReservationRestaurant/ReservationRestaurant/Data/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationRestaurant.Data
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        //public List<Restaurant> Restaurants { get; set; }
+        public List<Area> Areas { get; set; }
+
+    }
+}

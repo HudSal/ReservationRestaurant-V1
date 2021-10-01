719c212f964ca8eea6028c3a573be965660da60a
Huda Abusal <86791870+HudSal@users.noreply.github.com>
2021-10-01 22:41:59 +1000

    Update Reservation.cs

--- ReservationRestaurant/ReservationRestaurant/Data/Reservation.cs
+++ ReservationRestaurant/ReservationRestaurant/Data/Reservation.cs
@@ -10,24 +10,15 @@ namespace ReservationRestaurant.Data
         public int Duration { get; set; } // in miuntes
         public DateTime EndTime { get => StartTime.AddMinutes(Duration); }
         public int Guests { get; set; }
-
-
         public List<Table> Tables { get; set; } = new List<Table>();
-
         public int ReservationStatusId { get; set; }
         public ReservationStatus ReservationStatus { get; set; }
-
         public int ReservationOriginId { get; set; }
         public ReservationOrigin ReservationOrigin { get; set; }
-
-
         public int SittingId { get; set; }
         public Sitting Sitting { get; set; }
-
         public int PersonId { get; set; }
         public Person Person { get; set; }
-        
         public string SpecialRequirement {get; set;}
-
     }
 }

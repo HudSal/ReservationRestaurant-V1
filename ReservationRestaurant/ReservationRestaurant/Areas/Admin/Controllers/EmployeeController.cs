0b480b5ad04c068f634dda6d0c29bb998cae985f
CraigGuthers <86211377+CraigGuthers@users.noreply.github.com>
2021-10-04 16:29:32 +1000

    Add Employee Controller

---
+++ ReservationRestaurant/ReservationRestaurant/Areas/Admin/Controllers/EmployeeController.cs
@@ -0,0 +1,260 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReservationRestaurant.Data;
+using ReservationRestaurant.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationRestaurant.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Manager")]
+    public class EmployeeController : AdminAreaBaseController
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly PersonService _personService;
+        public EmployeeController(ApplicationDbContext context, UserManager<IdentityUser> userManager, PersonService personService) : base(context)
+        {
+            _userManager = userManager;
+            _personService = personService;
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Models.Employee.Update, Data.Person>().ReverseMap());
+            _mapper = new Mapper(config);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var person = new Person();
+            var emp = new List<Person>();           
+            var users = await _userManager.GetUsersInRoleAsync("Employee"); //get all users in Employee role 
+            var people = await _context.People.ToListAsync(); //get all people from people table 
+            foreach (var item in users)
+            {
+                person = people.Where(p => p.UserId == item.Id).FirstOrDefault(); //compare user Id 
+                //person = await _context.People.Where(p => p.UserId == item.Id).SingleOrDefaultAsync();
+                emp.Add(person); //add to list 
+            }
+            return View(emp);
+
+        }
+
+        [HttpGet]
+        public  IActionResult Create()
+        {
+            var m = new Models.Employee.Create();
+            return View(m);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Models.Employee.Create m )
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = m.Email, PhoneNumber = m.Phone, Email = m.Email };
+                var result = await _userManager.CreateAsync(user, m.Password);
+                if(result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, "Employee");
+                    var p = new Person
+                    {
+                        FirstName = m.FirstName,
+                        LastName = m.LastName,
+                        UserId = user.Id,
+                        Email = m.Email,
+                        Phone = m.Phone
+                       
+                    };
+                    await _personService.UpsertPersonAsync(p, true);
+                    return RedirectToAction("Index", "Home", new { area = "Admin" });
+                    
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+               
+            }
+            //If we got this far, something failed, redisplay form
+            return View(m);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return NotFound();
+            }
+            var person = await _context.People.FirstOrDefaultAsync(p => p.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            var m = _mapper.Map<Models.Employee.Update>(person);
+            return View(m);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, Models.Employee.Update m)
+        {
+
+            if (id != m.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var p = _mapper.Map<Data.Person>(m);
+                    _context.Update<Person>(p);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    //log the exception
+
+                }
+            }
+
+            return View(m);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateRoleToManager(int? id)
+        {
+            try
+            {
+                if (!id.HasValue)
+                {
+                    return StatusCode(400, "Id required");
+                }
+                var person = await _context.People.FirstOrDefaultAsync(p => p.Id == id.Value);
+                if (person == null)
+                {
+                    return NotFound();
+                }
+                return View(person);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+
+        }
+
+
+        [HttpPost]
+        [ActionName("UpdateRoleToManager")]
+        public async Task<IActionResult> UpdateRoleToManager(string userId)
+        {
+
+           var user =  await _userManager.FindByIdAsync(userId);
+           await _userManager.AddToRoleAsync(user, "Manager");
+           return RedirectToAction(nameof(Index));
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> RemoveManagerRole(int? id)
+        {
+            try
+            {
+                if (!id.HasValue)
+                {
+                    return StatusCode(400, "Id required");
+                }
+                var person = await _context.People.FirstOrDefaultAsync(p => p.Id == id.Value);
+                if (person == null)
+                {
+                    return NotFound();
+                }
+                return View(person);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+
+        }
+
+
+        [HttpPost]
+        [ActionName("RemoveManagerRole")]
+        public async Task<IActionResult> RemoveManagerRole(string userId)
+        {
+
+            var user = await _userManager.FindByIdAsync(userId);
+            await _userManager.RemoveFromRoleAsync(user, "Manager");
+            await _userManager.AddToRoleAsync(user, "Employee");
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            try
+            {
+                if (!id.HasValue)
+                {
+                    return StatusCode(400, "Id required");
+                }
+                var person = await _context.People.FirstOrDefaultAsync(p => p.Id == id.Value);
+                if (person == null)
+                {
+                    return NotFound();
+                }
+                return View(person);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            try
+            {
+                if (!id.HasValue)
+                {
+                    return StatusCode(400, "Id required");
+                }
+               
+                var person = await _context.People.FirstOrDefaultAsync(p => p.Id == id.Value); //get the person with associated id
+               
+                var users = await _userManager.FindByIdAsync(person.UserId.ToString()); //use the person from people table to get the user 
+                if (person == null)
+                {
+                    return NotFound();
+                }
+
+                
+                await _userManager.RemoveFromRoleAsync(users, "Employee");//remove the user from the role then delete altogether
+                await _userManager.DeleteAsync(users);
+                _context.People.Remove(person);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                //log the exp
+            }
+            return RedirectToAction("Delete", new { id });
+
+        }
+
+
+    }
+}
